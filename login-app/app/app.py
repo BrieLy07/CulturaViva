e1fb174424ab6181986546024934c12d5d066a9e
AndyChiquin <andychiquindev@gmail.com>
2025-07-23 23:07:26 -0500

    modificamos dockerfile y app.p

--- login-app/app/app.py
+++ login-app/app/app.py
@@ -98,10 +98,11 @@ clases_conflictivas = ['cañari', 'cayambis', 'puruhua', 'salasacas']
     
 def get_db_connection():
     return mysql.connector.connect(
-        host="db",
-        user="root",
-        password="root",
-        database="login_db"
+        host=os.getenv("DB_HOST"),
+        port=int(os.getenv("DB_PORT")),  
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME")
     )
 
 @app.route('/')
